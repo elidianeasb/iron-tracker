23f936b41a1ff1a1c528d69140b5f1fc60fd1d4a
Rafael <rafael.f.coelho@outlook.com>
2022-11-07 11:24:17 +0000

    Solved Joblist Populate

--- routes/job.routes.js
+++ routes/job.routes.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const Job = require('../models/Job.model');
 const router = express.Router();
+const User = require('../models/User.model')
 
 /* GET Create Job page */
 
@@ -9,10 +10,15 @@ router.get("/create", (req, res, next) => res.render("job/create"));
 
 router.post("/create", async (req, res, next) => {
     try {
+        const username = req.session.currentUser.username;
+        console.log(req.session.currentUser);
+        
         const {title, company, description, date, location, workplace, status, website, webUrl, contact, notes} = req.body;
 
         const createdJob = await Job.create({title, company, description, date, location, workplace, status, website, webUrl, contact, notes});
 
+        await User.findOneAndUpdate({username}, { $push: { jobList: createdJob._id } });
+        
         res.redirect(`/joblist`);
 
     } catch (error) {
--- routes/user.routes.js
+++ routes/user.routes.js
@@ -8,11 +8,12 @@ router.get("/joblist", async (req, res, next) => {
     console.log(req.session.currentUser._id);
     try {
         //const {id} = req.params;
-        const {id} = req.session.currentUser._id;
+        const id = req.session.currentUser._id;
         //const jobs = await Job.findById(id).populate('jobList');
         //TODO
         const jobs = await User.findById(id).populate('jobList');
-        res.render("user/main", {jobs});
+        console.log(jobs)
+        res.render("user/main", {jobs: jobs.jobList});
         //res.render("user/main");
     } catch (error) {
         console.log(error);
