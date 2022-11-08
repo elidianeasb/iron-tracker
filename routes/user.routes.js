efdccc30be541d7607cf8252ed3bb20d9e958de6
Rafael <rafael.f.coelho@outlook.com>
2022-11-08 18:10:17 +0000

    Implemented Dynamic Filter

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -18,14 +18,31 @@ router.get("/joblist", async (req, res, next) => {
 
         const jobsArray = jobs.jobList; //each element here is an OBJECT
 
-        if(req.query.filter){
+/*         if(req.query.filter){
             //! IF FILTER IS INSERTED
             let filterValue = req.query.filter;
 
             const filteredJobArray = jobsArray.filter(element => element.company == filterValue)
 
             res.render("user/main", {jobs: filteredJobArray});
-        };
+        }; */
+
+        //! -> Dynmaic Variant of If Filter is Inserted
+        if(req.query.filter){
+
+            let filterValue = req.query.filter;
+
+            const filterArrCompany = jobsArray.filter(element => element.company == filterValue);
+            const filterArrTitle = jobsArray.filter(element => element.title == filterValue);
+            const filterArrStatus = jobsArray.filter(element => element.status[0] == filterValue);
+
+            const filterArr = filterArrCompany.concat(filterArrTitle, filterArrStatus);
+
+            const filterSet = new Set(filterArr);
+
+            res.render("user/main", {jobs: filterSet});
+
+        }
 
         //! jobs is actually user
         res.render("user/main", {jobs: jobsArray});
