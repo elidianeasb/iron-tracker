4dbff2716ce859b3a2bd2a3c88011abd37d3840c
Rafael <rafael.f.coelho@outlook.com>
2022-11-08 13:49:01 +0000

    Implemented Filter btn on Joblist + edited Home

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -7,7 +7,6 @@ const Job = require('../models/Job.model');
 router.get("/joblist", async (req, res, next) => {
 
     try {
-
         //Validation Redirection for no Session
         if (!req.session.currentUser) {
             return res.render("auth/login");
@@ -15,17 +14,37 @@ router.get("/joblist", async (req, res, next) => {
 
         const id = req.session.currentUser._id;
 
-        //const jobs = await Job.findById(id).populate('jobList');
-        //TODO
         const jobs = await User.findById(id).populate('jobList');
-        console.log(jobs)
-        res.render("user/main", {jobs: jobs.jobList});
-        //res.render("user/main");
+
+        const jobsArray = jobs.jobList; //each element here is an OBJECT
+
+        if(req.query.filter){
+            //! IF FILTER IS INSERTED
+            let filterValue = req.query.filter;
+
+            const filteredJobArray = jobsArray.filter(element => element.company == filterValue)
+
+            res.render("user/main", {jobs: filteredJobArray});
+        };
+
+        //! jobs is actually user
+        res.render("user/main", {jobs: jobsArray});
+
     } catch (error) {
         console.log(error);
         next(error);
-    }
+    };
     
-  });
+});
+
+/* FILTER User Dashboard */
+
+/* router.get("/joblist/filter", async (req, res, next) => {
+    try {
+        const { filterValue } = req.query;
+    } catch (error) {
+        
+    }
+}) */
 
 module.exports = router;
